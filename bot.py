ec9a3791893f23b3ff44508c892f2918ac5518c5
Unknown <31830955+Altarrel@users.noreply.github.com>
2018-01-21 15:20:00 -0500

    added dbl posting

--- bot.py
+++ bot.py
@@ -8,9 +8,28 @@ I am a bot written by Altarrel to keep track of your Discord presence.
 """
 
 class TrackerBot(commands.Bot):
+    # Built off of this template: 
+    # https://github.com/SourSpoon/Discord.py-Template
     def __init__(self):
         super().init(command_prefix=commands.when_mentioned(), 
                      description=description)
+
+    async def load_startup_extensions(self):
+        """Attempts to load all .py files in /cogs/ as cog extensions
+        """
+
+        await self.wait_until_ready()
+        # Ensure that on_ready has completed and finished printing
+        await asyncio.sleep(1)
+        cogs = [x.stem for x in Path("cogs").glob("*.py")]
+        for extension in cogs:
+            try:
+                self.load_extension(f"cogs.{extension}")
+                print(f"Loaded {extension}")
+            except Exception as e:
+                error = f"{extension}\n {type(e).__name__} : {e}"
+                print(f"Failed to load extension {error}")
+            print("-" * 10)
 
     async def on_ready(self):
         print("LOGGED IN"
