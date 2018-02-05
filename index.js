eed2fc03ebf2660cc1194dfd19a22f87eb104d80
Unknown <31830955+Altarrel@users.noreply.github.com>
2018-02-05 12:54:40 -0500

    commands system

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const Discord = require("discord.js");
+
+class Bot extends Discord.Client {
+    get mention() {
+        return `<@${this.user.id}>`;
+    }
+
+    get prefix() {
+        return this.mention;
+    }
+
+    process_commands(message) {
+        // Only respond to explicitly invoked commands
+        if (!message.content.startsWith(this.prefix)) { return }
+
+        const args = message.content.slice(this.prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        if (commands[command]) {
+            commands[command].invoke(this, message, ...args);
+        } else if (command === "") {
+            // If the message was just the mention
+            commands["help"].invoke(this, message, ...args);
+        }
+    }
+}
+
+const bot = new Bot();
+
+bot.on("ready", () => {
+    console.log("Logged in:");
+    console.log("-".repeat(15));
+    console.log(`Username: ${bot.user.username}#${bot.user.discriminator}`);
+    console.log(`ID: ${bot.user.id}`);
+});
+
+bot.login(config.token);
